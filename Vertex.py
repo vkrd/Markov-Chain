f477467dfd8627d8b10b73cf42733e5a019a0823
vkrd <49703203+vkrd@users.noreply.github.com>
2019-09-02 12:07:04 -0500

    Comments

--- Vertex.py
+++ Vertex.py
@@ -7,18 +7,27 @@ class Vertex:
         self.word_counts = {}
 
     def add_node(self, consec_word):
+        
         if consec_word in self.word_counts:
+            # if word is already connected, increase frequency
             self.word_counts[consec_word] = self.word_counts[consec_word] + 1
         else:
+            # if word is not connected, add it to connected Vertices
             self.word_counts[consec_word] = 1
+
+        # update total to make probability calculation easier
         self.total += 1
 
     def generate_next_word(self):
+        # choose random Vertex from connected Vertices
         rand = random.randint(1,self.total)
+        
         for key, value in self.word_counts.items():
             rand -= value
             if rand <= 0:
                 return key
+
+        # if word has no outgoing edges, return a period
         return "."
 
     def getWords(self):
